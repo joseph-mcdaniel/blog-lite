70a9fb839a8c5c5e867d8e1cb408d39829aa6019
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2017-12-20 11:46:45 -0800

    add component for new posts

---
+++ src/components/posts_new.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PostsNew extends Component {
+  render() {
+    return (
+      <div>
+        New Posts
+      </div>
+    );
+  }
+}
+
+export default PostsNew;
