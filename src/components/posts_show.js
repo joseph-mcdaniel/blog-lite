e461eda3e41b0f09f5e678fcd37ce3721f8080d2
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2018-01-05 14:49:29 -0800

    add loading message to handle data dependencies

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -8,12 +8,19 @@ class PostsShow extends Component {
     this.props.fetchPost(id);
   }
   render() {
+    const { post } = this.props;
+
+    if (!post) {
+      return <div>Loading...</div>;
+    }
     return (
       <div>
-        Posts Show
+        <h3>{post.title}</h3>
+        <h6>Categories: {post.categories}</h6>
+        <p>{post.content}</p>
       </div>
     );
-  };
+  }
 }
 
 
@@ -21,4 +28,4 @@ function mapStateToProps({ posts }, ownProps) {
   return { post: posts[ownProps.match.params.id] };
 }
 
-export default connect(mapStateToProps, { fetchPost })PostsShow;
+export default connect(mapStateToProps, { fetchPost })(PostsShow);
